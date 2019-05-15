9911d03c63fb810218824fa8ad27d80609ac822a
Gavin Rehkemper <grehkemper@esri.com>
2019-05-15 13:03:40 -0500

    refactor

--- src/components/CameraDetails.tsx
+++ src/components/CameraDetails.tsx
@@ -19,39 +19,44 @@ declare global {
 }
 
 export default function CameraDetails({ camera }: CameraProps) {
-  const styleTemplate = raw("../templates/style.css");
-  const htmlHeadTemplate = raw("../templates/head.html");
-  const htmlTemplate = raw("../templates/html.html");
-  const jsTemplate = raw("../templates/js.txt");
+  
 
   const copyTextArea = useRef<HTMLTextAreaElement>(null);
   const [calculatedFullRequest, setCalculatedFullRequest] = useState('');
 
   useEffect(() => {
+    console.log('useEffect');
+
+    function createCodepen(cameraJsCode: object) {
+      console.log('createCodepen', cameraJsCode);
+      const styleTemplate = raw("../templates/style.css");
+      const htmlHeadTemplate = raw("../templates/head.html");
+      const htmlTemplate = raw("../templates/html.html");
+      const jsTemplate = raw("../templates/js.txt");
+      var jt = beautify.js_beautify(jsTemplate.replace('[CAMERAHERE]', JSON.stringify(cameraJsCode)), { indent_size: 2, space_in_empty_paren: true });
+  
+      var data = {
+        editors: "001",
+        html: htmlTemplate,
+        css: styleTemplate,
+        js: jt,
+        head: htmlHeadTemplate,
+        js_external: 'https://js.arcgis.com/4.11/dojo/dojo.js',
+        css_external: 'https://js.arcgis.com/4.11/esri/css/main.css;https://s3-us-west-1.amazonaws.com/patterns.esri.com/files/calcite-web/1.0.2/css/calcite-web.min.css',
+      };
+  
+      var JSONstring = 
+        JSON.stringify(data);
+          // Quotes will screw up the JSON
+          // .replace(/"/g, "&​quot;") // careful copy and pasting, I had to use a zero-width space here to get markdown to post this.
+          // .replace(/'/g, "&apos;");
+  
+      return JSONstring;
+    }
     setCalculatedFullRequest(createCodepen(camera));
   }, [camera]);
 
-  function createCodepen(cameraJsCode: object) {
-    var jt = beautify.js_beautify(jsTemplate.replace('[CAMERAHERE]', JSON.stringify(cameraJsCode)), { indent_size: 2, space_in_empty_paren: true });
-
-    var data = {
-      editors: "001",
-      html: htmlTemplate,
-      css: styleTemplate,
-      js: jt,
-      head: htmlHeadTemplate,
-      js_external: 'https://js.arcgis.com/4.11/dojo/dojo.js',
-      css_external: 'https://js.arcgis.com/4.11/esri/css/main.css;https://s3-us-west-1.amazonaws.com/patterns.esri.com/files/calcite-web/1.0.2/css/calcite-web.min.css',
-    };
-
-    var JSONstring = 
-      JSON.stringify(data);
-        // Quotes will screw up the JSON
-        // .replace(/"/g, "&​quot;") // careful copy and pasting, I had to use a zero-width space here to get markdown to post this.
-        // .replace(/'/g, "&apos;");
-
-    return JSONstring;
-}
+  
 
   function copyJsonButtonClickHandler() {
     if (copyTextArea && copyTextArea.current) {
